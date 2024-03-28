c6637e2719234cb52b924583aedd99069fe19332
Ayose <ayosec@gmail.com>
2024-03-28 00:00:00 +0000

    Script in nix-shell to launch the tests.
    It relies on `watchexec` to relaunch the tests on any update, and it can be
    invoked with `nix-shell --run watch`.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,34 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
+let
+
+  make-all = pkgs.writeShellScriptBin "make-all" ''
+    if make fmt all
+    then
+        c=42
+    else
+        c=41
+    fi
+
+    printf '\e[%dm\e[K\e[m\n' $c
+    test $c -ne 41
+  '';
+
+  watch = pkgs.writeShellScriptBin "watch" ''
+    exec ${pkgs.watchexec}/bin/watchexec --restart --quiet -n \
+      -- ${make-all}/bin/make-all
+  '';
+
+in
 pkgs.mkShell {
 
   nativeBuildInputs = with pkgs; [
+    gnumake
     lua-language-server
     luajitPackages.luacheck
     ripgrep
     stylua
+    watch
+    watchexec
   ];
 
 }
