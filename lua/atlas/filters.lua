b3a39bb079a1cf10d4239aba86062241489b49f0
Ayose <ayosec@gmail.com>
2024-03-28 00:00:00 +0000

    Support more filters in the parser.
    - Negate filters with `-`.
    - Filter by file contents with `/`.
    - `*` and `.` are adjusted in simple specifiers.

--- lua/atlas/filters.lua
+++ lua/atlas/filters.lua
@@ -1,25 +1,57 @@
 local M = {}
 
+--- Adjust a specifier to replace `.` with `\.`, and `*` with `.*`.
+---
+---@param spec string
+---@return string
+local function adjust_regex(spec)
+    local s = string.gsub(spec, "%.", "\\.")
+    s = string.gsub(s, "*", ".*")
+
+    return s
+end
+
 ---@enum kind
 M.FilterKind = {
     Simple = 1,
+    FileContents = 2,
 }
 
+--- Represent a specifier in a filter.
+---
 ---@class FilterSpec
 ---@field kind kind
+---@field negated boolean
 ---@field value string
 
----Parse a filter and return a list with each specifier.
+--- Parse a filter and return a list with each specifier.
+---
 ---@param filter string
 ---@return FilterSpec[]
 M.parse = function(filter)
+    ---@type FilterSpec[]
     local specs = {}
 
     for spec in string.gmatch(filter, "%S+") do
-        table.insert(specs, {
-            kind = M.FilterKind.Simple,
-            value = spec,
-        })
+        ---@type FilterSpec
+        local new_spec = {}
+
+        if vim.startswith(spec, "-") then
+            new_spec.negated = true
+            spec = spec:sub(2)
+        else
+            new_spec.negated = false
+        end
+
+        if vim.startswith(spec, "/") then
+            new_spec.kind = M.FilterKind.FileContents
+            new_spec.value = spec:sub(2)
+        else
+            new_spec.kind = M.FilterKind.Simple
+            new_spec.value = adjust_regex(spec)
+        end
+
+        table.insert(specs, new_spec)
     end
 
     return specs
--- tests/specs/filters_spec.lua
+++ tests/specs/filters_spec.lua
@@ -7,9 +7,11 @@ describe("Filters", function()
         local specs = filters.parse("foo bar")
 
         assert_eq(specs[1].kind, filters.FilterKind.Simple)
+        assert_eq(specs[1].negated, false)
         assert_eq(specs[1].value, "foo")
 
         assert_eq(specs[2].kind, filters.FilterKind.Simple)
+        assert_eq(specs[2].negated, false)
         assert_eq(specs[2].value, "bar")
 
         assert_eq(#specs, 2)
@@ -20,6 +22,44 @@ describe("Filters", function()
 
         assert_eq(specs[1].value, "foo")
         assert_eq(specs[2].value, "bar")
+        assert_eq(#specs, 2)
+    end)
+
+    it("adjust regexs", function()
+        local specs = filters.parse("foo bar*.lua")
+
+        assert_eq(specs[1].kind, filters.FilterKind.Simple)
+        assert_eq(specs[1].value, "foo")
+
+        assert_eq(specs[2].kind, filters.FilterKind.Simple)
+        assert_eq(specs[2].value, "bar.*\\.lua")
+
+        assert_eq(#specs, 2)
+    end)
+
+    it("find by file contents", function()
+        local specs = filters.parse("foo /bar.*\\d")
+        assert_eq(specs[1].kind, filters.FilterKind.Simple)
+        assert_eq(specs[1].negated, false)
+        assert_eq(specs[1].value, "foo")
+
+        assert_eq(specs[2].kind, filters.FilterKind.FileContents)
+        assert_eq(specs[2].negated, false)
+        assert_eq(specs[2].value, "bar.*\\d")
+
+        assert_eq(#specs, 2)
+    end)
+
+    it("negate specifiers", function()
+        local specs = filters.parse("-foo -/bar")
+        assert_eq(specs[1].kind, filters.FilterKind.Simple)
+        assert_eq(specs[1].negated, true)
+        assert_eq(specs[1].value, "foo")
+
+        assert_eq(specs[2].kind, filters.FilterKind.FileContents)
+        assert_eq(specs[2].negated, true)
+        assert_eq(specs[2].value, "bar")
+
         assert_eq(#specs, 2)
     end)
 end)
