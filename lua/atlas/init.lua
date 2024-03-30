d2349c94727e56bb682543af3f447e924e83dd40
Ayose <ayosec@gmail.com>
2024-03-30 00:00:00 +0000

    Default configuration.

--- lua/atlas/init.lua
+++ lua/atlas/init.lua
@@ -1,3 +1,37 @@
 local M = {}
 
+M.namespace = vim.api.nvim_create_namespace("atlas")
+
+function M.defaults()
+    ---@class AtlasConfig
+    local defaults = {
+        user_command = "Atlas",
+
+        programs = {
+            ripgrep = "rg",
+            xargs = "xargs",
+        },
+
+        files = {
+            exclude_always = { ".git" },
+
+            hidden = true,
+        },
+
+        view = {
+            max_lines = 30,
+        },
+    }
+
+    return defaults
+end
+
+--- @type AtlasConfig
+M.options = {}
+
+---@param opts? AtlasConfig
+function M.setup(opts)
+    M.options = vim.tbl_deep_extend("force", {}, M.defaults(), opts or {})
+end
+
 return M
