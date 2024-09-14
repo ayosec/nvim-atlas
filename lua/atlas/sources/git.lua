36e56d155cf6fffcf34495ebdf44079eb1f43d08
Ayose <ayosec@gmail.com>
2024-09-14 00:00:00 +0000

    Always run `git diff` in the root directory of the repository.
    This is needed to get the correct paths when Neovim is running in a
    subdirectory of the repository.

--- lua/atlas/sources/git.lua
+++ lua/atlas/sources/git.lua
@@ -1,11 +1,37 @@
 local M = {}
 
+---@param search_dir string
+---@return string?
+local function git_root(search_dir)
+    local matches = vim.fs.find(".git", {
+        path = search_dir,
+        upward = true,
+    })
+
+    if #matches == 0 then
+        return nil
+    end
+
+    return vim.fs.dirname(matches[1])
+end
+
 ---@param finder atlas.Finder
 ---@param argument string|nil
 ---@param search_dir string
 ---@return atlas.sources.Response
 function M.gitdiff(finder, argument, search_dir)
     local rev
+
+    -- Run the command from the root directory of the repository.
+    local root = git_root(search_dir)
+
+    if not root then
+        -- Empty list if `search_dir` is not a git repository.
+        return {
+            search_dir = search_dir,
+            files = {},
+        }
+    end
 
     -- The source argument is used to use a specific revision
     -- to compare.
@@ -21,7 +47,7 @@ function M.gitdiff(finder, argument, search_dir)
     end
 
     return {
-        search_dir = search_dir,
+        search_dir = root,
         filelist_command = vim.list_extend(
             vim.list_extend({ "git", "diff", "--name-only", "-z" }, rev),
             { "--", search_dir }
